6e1db1bf537195a38f516f09c3e84a41ffdc89ea
Neil Trotter <ntrotter699@gmail.com>
2019-08-26 10:54:21 +0100

    Prepare for Matomo integration

--- app/src/main/java/pw/dvd604/music/MusicApplication.kt
+++ app/src/main/java/pw/dvd604/music/MusicApplication.kt
@@ -3,16 +3,39 @@ package pw.dvd604.music
 import android.app.Application
 import io.sentry.Sentry
 import io.sentry.android.AndroidSentryClientFactory
+import org.matomo.sdk.Matomo
+import org.matomo.sdk.Tracker
+import org.matomo.sdk.TrackerBuilder
+import org.matomo.sdk.extra.TrackHelper
 import pw.dvd604.music.util.Settings
 import kotlin.system.exitProcess
 
 
 class MusicApplication : Application() {
 
+    companion object {
+        private var tracker: Tracker? = null
+
+        fun track(category: String, event: String) {
+            if (tracker != null) {
+                TrackHelper.track().event(category, event).with(tracker)
+            }
+        }
+    }
+
     override fun onCreate() {
         super.onCreate()
 
+        //here is where we initialise everything that needs a context, but we want to keep out of the main activity for neatness sake
+
         Settings.init(this)
+
+        if (Settings.getBoolean(Settings.tracking, true)) {
+            tracker = TrackerBuilder.createDefault(BuildConfig.apiURL, BuildConfig.siteID)
+                .build(Matomo.getInstance(this))
+            track("App Event", "Start")
+        }
+
 
         if (Settings.getBoolean(Settings.crashReports, true)) {
             Sentry.init(
