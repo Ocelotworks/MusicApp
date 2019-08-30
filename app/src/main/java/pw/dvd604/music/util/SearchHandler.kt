87887c326e056a62a51052f9c11d1a4bd65d9a66
Neil Trotter <ntrotter699@gmail.com>
2019-08-30 19:02:29 +0100

    Again...

--- app/src/main/java/pw/dvd604/music/util/SearchHandler.kt
+++ app/src/main/java/pw/dvd604/music/util/SearchHandler.kt
@@ -1,11 +1,9 @@
 package pw.dvd604.music.util
 
-import pw.dvd604.music.adapter.data.Song
-
 class SearchHandler {
     companion object {
-        fun search(query: String): ArrayList<Song> {
-            return null
-        }
+        /* fun search(query: String): ArrayList<Song> {
+             return null
+         }*/
     }
 }
