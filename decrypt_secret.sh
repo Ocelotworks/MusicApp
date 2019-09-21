525bf26f14fe9ae949cad79bd239c6009c1ec6c4
Neil Trotter <ntrotter699@gmail.com>
2019-09-21 20:00:49 +0100

    Update decrypt_secret.sh

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -3,5 +3,4 @@
 # Decrypt the file
 mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$APP_PROPS_SECRET" \
---output $HOME/app.props app.props.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$APP_PROPS_SECRET" --output $HOME/app.props app.props.gpg
