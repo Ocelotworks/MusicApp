9b05b65e5856baf0b3c764b4b46f58f2e5490605
Neil Trotter <ntrotter699@gmail.com>
2019-09-21 20:26:36 +0100

    Update decrypt_secret.sh

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -2,4 +2,4 @@
 
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --version
-gpg --verbose --batch --yes --decrypt --passphrase="$APP_PROPS_SECRET" --output $HOME/app.props app.props.gpg
+gpg --verbose --batch --yes --decrypt --passphrase="$APP_PROPS_SECRET" --output /home/runner/work/MusicApp/MusicApp/app.props app.props.gpg
